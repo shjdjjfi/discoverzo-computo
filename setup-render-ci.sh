a010dcc58b7b802efac4787e5bfbd487b83037e6
shjdjjfi <86753270+shjdjjfi@users.noreply.github.com>
2026-07-19 09:11:54 +0800

    Update setup-render-ci.sh

--- setup-render-ci.sh
+++ setup-render-ci.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
-quarto add computorg/computo-quarto-extension@v1.0.4 --no-prompt
-quarto render
+
+echo "Checking Python and Jupyter environment..."
+python --version
+python -m jupyter --version
+quarto check jupyter
